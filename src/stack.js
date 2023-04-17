d9eba7134534adb0bc7eca4fbe5f9768137951a3
Inga <inkic93@gmail.com>
2023-04-17 12:46:34 +0300

    feat: change stack

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,22 +13,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.item = [];
+  }
+
+  push(element) {
+    this.item.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.item.length === 0) return -1;
+    return this.item.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.item[this.item.length - 1];
+  }
+
+  isEmpty() {
+    return this.item.length === 0;
   }
 }
 
 module.exports = {
-  Stack
+  Stack,
 };
